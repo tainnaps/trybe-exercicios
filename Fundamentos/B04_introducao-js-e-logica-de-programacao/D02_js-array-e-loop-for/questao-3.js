5eb2a8bfb0b3eda13a6cff40bccec0c99d88e3f6
Taina <tainnaps@gmail.com>
2021-08-26 18:27:17 -0300

    Resolve questao 3

--- Fundamentos/B04_introducao-js-e-logica-de-programacao/D02_js-array-e-loop-for/questao-3.js
+++ Fundamentos/B04_introducao-js-e-logica-de-programacao/D02_js-array-e-loop-for/questao-3.js
@@ -1,10 +1,13 @@
-// Questão 2
+// Questão 3
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let average = 0;
 
 for (let element of numbers) {
   sum = sum + element;
 }
 
-console.log(sum);
+average = sum / numbers.length;
+
+console.log(average);
