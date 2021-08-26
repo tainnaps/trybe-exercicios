3b61ccde205caed3a98a0908c4636a3608285da5
Taina <tainnaps@gmail.com>
2021-08-26 02:23:11 -0300

    Resolve questao 9

--- Fundamentos/B04_introducao-js-e-logica-de-programacao/D01_js-primeiros-passos/questao-9.js
+++ Fundamentos/B04_introducao-js-e-logica-de-programacao/D01_js-primeiros-passos/questao-9.js
@@ -1,10 +1,10 @@
-// Questão 8
+// Questão 9
 
 const a = 5;
-const b = 3;
+const b = 4;
 const c = 2;
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
   console.log("true");
 }
 else {
