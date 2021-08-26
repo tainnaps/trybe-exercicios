19f4d696767b09e804a5810c821509206eaf2b6e
Taina <tainnaps@gmail.com>
2021-08-26 00:47:16 -0300

    Resolve questao 4

---
+++ Fundamentos/B04_introducao-js-e-logica-de-programacao/D01_js-primeiros-passos/questao-4.js
@@ -0,0 +1,16 @@
+// Questao 4
+
+const a = 5;
+
+if (a > 0) {
+  console.log("Positive");
+}
+else if (a < 0) {
+  console.log("Negative");
+}
+else if (a === 0) {
+  console.log("Zero");
+}
+else {
+  console.log("Valor inválido");
+}
