74cf0897dd915bba7a1259656c63b5dbbb8f799b
Taina <tainnaps@gmail.com>
2021-08-30 18:55:37 -0300

    Resolve questao 3 da parte 1

--- Fundamentos/B04_introducao-js-e-logica-de-programacao/D04_js-objetos-e-funcoes/part-1.js
+++ Fundamentos/B04_introducao-js-e-logica-de-programacao/D04_js-objetos-e-funcoes/part-1.js
@@ -1,10 +1,18 @@
+//Questão 1
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
+//console.log("Bem-vinda,", info.personagem);
+
+// Questão 2
 info.recorrente = "Sim";
 
-console.log("Bem-vinda,", info.personagem);
-console.log(info);
+//console.log(info);
+
+// Questão 3
+for (const key in info) {
+  console.log(key);
+}
