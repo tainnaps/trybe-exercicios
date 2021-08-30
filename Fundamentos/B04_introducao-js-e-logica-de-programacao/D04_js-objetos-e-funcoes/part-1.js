32858ab3fe7497e3b1a5c0f0f3308d65d54a3a91
Taina <tainnaps@gmail.com>
2021-08-30 18:41:45 -0300

    Resolve questao 2 da parte 1

--- Fundamentos/B04_introducao-js-e-logica-de-programacao/D04_js-objetos-e-funcoes/part-1.js
+++ Fundamentos/B04_introducao-js-e-logica-de-programacao/D04_js-objetos-e-funcoes/part-1.js
@@ -1,7 +1,10 @@
 let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
+info.recorrente = "Sim";
+
 console.log("Bem-vinda,", info.personagem);
+console.log(info);
