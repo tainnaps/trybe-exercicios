cadfc1c268906759350d7f147ff57438d7b9fd0c
Taina <tainnaps@gmail.com>
2021-09-09 05:11:47 -0300

    Resolve exercicio 1

--- Fundamentos/B05_js-dom-eventos-e-web-storage/D03_js-eventos/script.js
+++ Fundamentos/B05_js-dom-eventos-e-web-storage/D03_js-eventos/script.js
@@ -14,3 +14,27 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Exercício 1
+const daysList = document.getElementById('days');
+
+function addDezDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    listItem = document.createElement('li');
+    listItem.innerText = dezDaysList[index];
+    listItem.className = 'day';
+    daysList.appendChild(listItem);
+
+    if (listItem.innerText === '24' || listItem.innerText === '25' || listItem.innerText === '31') {
+      listItem.classList.add('holiday');
+    }
+
+    if (listItem.innerText === '4' || listItem.innerText === '11' || listItem.innerText === '18' || listItem.innerText === '25') {
+      listItem.classList.add('friday');
+    }
+  }
+}
+
+addDezDays();
