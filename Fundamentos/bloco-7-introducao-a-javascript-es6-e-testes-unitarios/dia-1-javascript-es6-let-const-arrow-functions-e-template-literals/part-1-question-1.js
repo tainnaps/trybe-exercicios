b474068a62e379ee29b73755e5e05c330080c51e
Taina <tainnaps@gmail.com>
2021-09-23 18:39:55 -0300

    Resolve questao 1 da parte 1

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/part-1-question-1.js
@@ -0,0 +1,13 @@
+// Part 1 - question 1
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope}. Ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+};
+
+testingScope(true);
