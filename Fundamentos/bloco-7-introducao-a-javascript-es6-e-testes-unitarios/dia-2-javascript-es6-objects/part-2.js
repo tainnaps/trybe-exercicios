8b3c7e432dde2d3044cc13e6e72f5280608bdb71
Taina <tainnaps@gmail.com>
2021-09-27 14:24:33 -0300

    Resolve questao 1 da parte 2

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/part-2.js
@@ -0,0 +1,47 @@
+// Part 2
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Question 1
+const addPairKeyValue = (object, key, value) => { object[key] = value; };
+
+addPairKeyValue(lesson2, 'turno', 'noturno');
+
+// console.log(lesson2);
+
+// Question 2
+
+
+// Question 3
+
+
+// Question 4
+
+
+// Question 5
+
+
+// Question 6
+
+
+// Question 7
+
+
+// Question 8
