05bfc9c0e46b5b0559a102bd9bfb86209b061c92
Taina <tainnaps@gmail.com>
2021-09-27 12:49:33 -0300

    Resolve questao 1 da parte 1

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/part-1.js
@@ -0,0 +1,51 @@
+// Part 1
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+// Question 1
+const customerInfo = (order) => {
+  return `Hello Ana Silveira, delivery to: ${order.name}, phone number: ${order.phoneNumber}, ${order.address.street}, nº: ${order.address.number}, AP: ${order.address.apartment}`;
+};
+
+console.log(customerInfo(order));
+
+// Question 2
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+};
+
+orderModifier(order);
