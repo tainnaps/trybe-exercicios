a56db42f5eea81b033272471df8e7d2c75591e83
Taina <tainnaps@gmail.com>
2021-10-07 19:35:44 -0300

    Resolve exercicio 1

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,16 @@
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten(matrix) {
+  const reduced = matrix.reduce((accumulator, currentValue) => {
+    accumulator.push(...currentValue);
+    return accumulator;
+  }, []);
+
+  return reduced;
+}
+
+console.log(flatten(arrays));
