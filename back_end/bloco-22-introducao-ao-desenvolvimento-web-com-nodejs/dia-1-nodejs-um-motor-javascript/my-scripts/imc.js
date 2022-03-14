bbcc0a4593c24b55bf40b24fe06b05c49c4d9732
Taina Santana <tainnaps@gmail.com>
2022-03-14 11:47:02 -0300

    refactor: use one function to calculate imc

---
+++ back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -0,0 +1,28 @@
+const readlineSync = require('readline-sync');
+
+const calculateIMC = () => {
+  let userStatus = '';
+  const userName = readlineSync.question('What is your name? ');
+  const userWeight = readlineSync.questionFloat('What is your weight? ');
+  const userHeight = readlineSync.questionFloat('What is your height? ');
+
+  const userIMC = (userWeight / (userHeight ** 2)).toFixed(2);
+
+  if (userIMC < 18.5) {
+    userStatus = 'under weight';
+  } else if (userIMC >= 18.5 && userIMC <= 24.9) {
+    userStatus = 'normal weight';
+  } else if (userIMC >= 25.0 && userIMC <= 29.9) {
+    userStatus = 'overweight';
+  } else if (userIMC >= 30.0 && userIMC <= 34.9) {
+    userStatus = 'obesity level I';
+  } else if (userIMC >= 35.0 && userIMC <= 39.9) {
+    userStatus = 'obesity level II';
+  } else {
+    userStatus = 'obesity levels III e IV';
+  }
+
+  console.log(`Hello, ${userName}! Your IMC is ${userIMC} and you have ${userStatus}.`);
+}
+
+calculateIMC();
