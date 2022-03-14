0cb7238a59c84e3c2fe92910db644e1dc2f520f6
Taina Santana <tainnaps@gmail.com>
2022-03-14 12:05:45 -0300

    feat: create velocity calculator

---
+++ back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/velocity.js
@@ -0,0 +1,11 @@
+const readlineSync = require('readline-sync');
+
+const calculateVelocity = () => {
+  const distance = readlineSync.questionInt('Whats is the travelled distance by the car (m)? ');
+  const time = readlineSync.questionInt('In how much time did the car travelled this distance (s)? ');
+  const velocity = (distance / time).toFixed(2);
+
+  console.log(`The car average velocity is ${velocity}.`);
+}
+
+calculateVelocity();
