ba5c8f3b3cbf0140fa6d65a11bb7fa8cfb8f6367
Taina Santana <tainnaps@gmail.com>
2022-03-14 11:18:09 -0300

    feat: use readline-sync on imc calculator

--- back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc/imc.js
+++ back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc/imc.js
@@ -1,5 +1,9 @@
-const weight = 50;
-const height = 1.63;
+const readlineSync = require('readline-sync');
+
+const name = readlineSync.question('What is your name? ');
+const weight = readlineSync.questionFloat('What is your weight? ');
+const height = readlineSync.questionFloat('What is your height? ');
+
 const imc = (weight, height) => (weight / (height ** 2)).toFixed(2);
 
-console.log(imc(weight, height));
+console.log(`Hello, ${name}! Your IMC is ${imc(weight, height)}`);
