d696bc51657b94205506c40d9f2258a5b7ebb510
Tainá Santana <tainnaps@gmail.com>
2022-05-09 19:27:21 -0300

    feat: exercise 2

---
+++ back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/exercise-2.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.months = void 0;
+var months;
+(function (months) {
+    months["JANUARY"] = "January";
+    months["FEBRUARY"] = "February";
+    months["MARCH"] = "March";
+    months["APRIL"] = "April";
+    months["MAY"] = "May";
+    months["JUNE"] = "June";
+    months["JULY"] = "July";
+    months["AUGUST"] = "August";
+    months["DEPTEMBER"] = "September";
+    months["OCTOBER"] = "October";
+    months["NOVEMBER"] = "November";
+    months["DECEMBER"] = "December";
+})(months = exports.months || (exports.months = {}));
---
+++ back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/exercise-2.ts
@@ -0,0 +1,14 @@
+export enum months {
+  JANUARY = 'January',
+  FEBRUARY = 'February',
+  MARCH = 'March',
+  APRIL = 'April',
+  MAY = 'May',
+  JUNE = 'June',
+  JULY = 'July',
+  AUGUST = 'August',
+  DEPTEMBER = 'September',
+  OCTOBER = 'October',
+  NOVEMBER = 'November',
+  DECEMBER = 'December',
+}
