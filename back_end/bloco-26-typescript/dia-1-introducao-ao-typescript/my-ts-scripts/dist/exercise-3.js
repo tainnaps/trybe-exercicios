c17ed996927fc7f9c0a6ef7ae46535792647ed2b
Tainá Santana <tainnaps@gmail.com>
2022-05-09 19:27:30 -0300

    feat: exercise 3

---
+++ back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/exercise-3.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.seasons = void 0;
+var seasons;
+(function (seasons) {
+    seasons["SUMMER"] = "Summer";
+    seasons["WINTER"] = "Winter";
+    seasons["AUTUMN"] = "Autumn";
+    seasons["SPRING"] = "Spring";
+})(seasons = exports.seasons || (exports.seasons = {}));
---
+++ back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/exercise-3.ts
@@ -0,0 +1,6 @@
+export enum seasons {
+  SUMMER = 'Summer',
+  WINTER = 'Winter',
+  AUTUMN = 'Autumn',
+  SPRING = 'Spring',
+}
