4209951db9c5e311b3b57c6b99c3dd1c3105490e
Tainá Santana <tainnaps@gmail.com>
2022-05-10 11:19:44 -0300

    refactor: change seasons enum name

--- back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/exercise-3.js
+++ back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/exercise-3.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.seasons = void 0;
-var seasons;
-(function (seasons) {
-    seasons["SUMMER"] = "Summer";
-    seasons["WINTER"] = "Winter";
-    seasons["AUTUMN"] = "Autumn";
-    seasons["SPRING"] = "Spring";
-})(seasons = exports.seasons || (exports.seasons = {}));
+exports.Seasons = void 0;
+var Seasons;
+(function (Seasons) {
+    Seasons["SUMMER"] = "Summer";
+    Seasons["WINTER"] = "Winter";
+    Seasons["AUTUMN"] = "Autumn";
+    Seasons["SPRING"] = "Spring";
+})(Seasons = exports.Seasons || (exports.Seasons = {}));
--- back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/exercise-3.ts
+++ back_end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/exercise-3.ts
@@ -1,4 +1,4 @@
-export enum seasons {
+export enum Seasons {
   SUMMER = 'Summer',
   WINTER = 'Winter',
   AUTUMN = 'Autumn',
