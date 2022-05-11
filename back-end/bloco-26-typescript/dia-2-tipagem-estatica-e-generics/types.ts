eca518d033b991c0fb435661d222485103e5a7c0
Tainá Santana <tainnaps@gmail.com>
2022-05-11 10:13:45 -0300

    feat: create Car class and its used types

---
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/types.ts
@@ -0,0 +1,18 @@
+export enum Colors {
+  BLACK = 'Black',
+  RED = 'Red',
+  WHITE = 'White',
+  SILVER = 'Silver',
+}
+
+export enum Doors {
+  DRIVER = 'driver\'s door',
+  RIDE = 'ride\'s door',
+  BEHIND_DRIVER = 'behind driver\'s door',
+  BEHIND_RIDE = 'behind ride\'s door',
+}
+
+export enum Directions {
+  RIGHT = 'right',
+  LEFT = 'left',
+}
